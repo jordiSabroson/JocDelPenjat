b80a89a77cda731f16e7b92e6f8573e85ac354ca
Jordi Sabroson <2023_jordi.manya@iticbcn.cat>
2023-11-17 16:22:15 +0100

    lògica casi acabada i imatges afegides

--- JocDelPenjatGrafic.js
+++ JocDelPenjatGrafic.js
@@ -8,43 +8,79 @@ let lletresFallades = [];
 // Variable amb els intents màxims del joc
 let max = 6;
 
+let paraula = "";
+let endevina = "";
+let errors = "";
+let botons = '';
+
+let be = 0, malament = 0;
+
+let lletraCorrecta = false;
+
 // Variables que emmagatzemen localment les partides guanyades, perdudes i jugades (les inicialitza a 0 si no existeixen)
 let guanyades = localStorage.getItem("guanyades") || 0;
 let perdudes = localStorage.getItem("perdudes") || 0;
 let jugades = localStorage.getItem("jugades") || 0;
 
+function valid(caracter) {
+    return caracter.match(/^[a-zA-ZàáèéìíòóùúÀÁÈÉÌÍÒÓÙÚüÜñÑçÇ]$/);
+}
+
+function validarParaula(paraula) {
+    return /^[^0-9\s]+$/.test(paraula) && paraula.length > 1;
+}
 
 function novaPartida() {
+    do {
+        paraula = prompt("Introdueix una paraula: ");
+    } while (!validarParaula(paraula));
+    paraula = paraula.toUpperCase();
+
     for (let i = 0; i < abc.length; i++) {
-        let button = document.createElement("button");
-        button.innerText = abc[i];
-        button.onclick = function() {
-            clickLletra(abc[i]);
-        };
-        document.querySelector("#abecedari").appendChild(button);
+        let char = abc[i];
+        botons += '<button id="' + char + '" onclick="clickLletra(\'' + char + '\')">' + char + '</button>';
     }
+    document.getElementById("abecedari").innerHTML = botons;
     document.querySelector("#novaPartida").remove();
     document.querySelector("#estadistiques").remove();
 
-    let paraula;
-    do {
-        paraula = prompt("Introdueix una paraula: ");
-    } while (paraula < 1)
-
-    let endevina = "";
     for (let i = 0; i < paraula.length; i++) {
-        endevina += "_ ";
+        endevina += "_";
     }
+
     document.getElementById("jocPenjat").innerHTML = endevina;
 
-    // do {
-        
-    // } while(a);
+    let imatge = document.getElementById("imatgePenjat");
+    imatge.src = "img/penjat_" + malament + ".png";
 
 }
 
 function clickLletra(lletra) {
-    return lletra;
+    do {
+        lletraCorrecta = false;
+    
+        for (let i = 0; i < paraula.length; i++) {
+            if (paraula[i] == lletra) {
+                lletraCorrecta = true;
+                endevina = endevina.substring(0, i) + lletra + endevina.substring((i + 1));
+            }
+        }
+    
+        if (lletraCorrecta) {
+            be++;
+            lletresEndevinades.push(lletra);
+            document.getElementById("jocPenjat").innerHTML = endevina;
+            document.getElementById(lletra).disabled = true;
+        } else {
+            lletresFallades.push(lletra);
+            malament++;
+            errors = "Errors: " + malament + "/" + max
+            document.getElementById("lletresUtilitzades").innerHTML = errors;
+            document.getElementById(lletra).disabled = true;
+
+        }
+    } while (be < paraula.length && malament < max);
+
 }
 
 function estadistiques() {
